2af1de3fe2580d91a259182600d21effdda25593
Cruzy20 <baltazar.arredondo@uabc.edu.mx>
2020-02-20 20:17:27 -0800

    SegundoCommit

--- src/main/java/com/example/demo/controller/CalculadoraAvzController.java
+++ src/main/java/com/example/demo/controller/CalculadoraAvzController.java
@@ -2,13 +2,14 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Operacion;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RequestMapping("/api")
 public class CalculadoraAvzController {
 
-    @PostMapping(name = "ops")
+    @PostMapping("ops")
     public Operacion operaciones(@RequestParam(name = "operacion") Operacion operacion) {
 
         switch  (operacion.getOp()) {
